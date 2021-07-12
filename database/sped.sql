cff4cdbcf8b7f9c3b7d223cc36c5d0f96a9325ef
angelogelo <angelon.nyelo@gmail.com>
2021-07-12 16:21:36 +0800

    Updated Database

--- database/sped.sql
+++ database/sped.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2021 at 03:46 PM
+-- Generation Time: Jul 02, 2021 at 08:03 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -88,8 +88,8 @@ CREATE TABLE `announcement` (
 --
 
 INSERT INTO `announcement` (`id`, `title`, `description`, `cms`, `created_at`) VALUES
-(3, 'Fogging', 'School Foggings', 'true', '2021-06-02 05:33:53'),
-(4, 'Asd', 'Asdas', 'true', '2021-06-02 05:33:54');
+(3, 'Fogging', 'School Foggings', 'false', '2021-07-02 03:12:15'),
+(4, 'Asd', 'Asdas', 'false', '2021-07-02 03:12:16');
 
 -- --------------------------------------------------------
 
@@ -131,8 +131,7 @@ CREATE TABLE `disability` (
 INSERT INTO `disability` (`id`, `assign_teacher`, `description`, `created_at`) VALUES
 (1, '2021', 'Visual Impairment', '2021-05-29 18:31:21'),
 (2, '2022', 'ADHD', '2021-06-02 04:15:34'),
-(3, '2023', 'Down Syndrome', '2021-05-29 18:31:36'),
-(4, 'Asd', 'Mute', '2021-05-31 04:15:59');
+(3, '2023', 'Down Syndrome', '2021-05-29 18:31:36');
 
 -- --------------------------------------------------------
 
@@ -200,9 +199,11 @@ CREATE TABLE `login_details` (
 --
 
 INSERT INTO `login_details` (`id`, `user_id`, `last_login`, `last_activity`) VALUES
-(23, 'LRN1', '1622878116', '2021-06-05 07:28:26'),
-(24, 'LRN2', '1622615821', '2021-06-02 06:36:51'),
-(25, 'wew', '1622434633', '2021-05-31 04:17:03');
+(23, 'LRN1', '1625192077', '2021-07-02 02:14:27'),
+(24, 'LRN2', '1625192366', '2021-07-02 02:19:16'),
+(25, 'wew', '1622434633', '2021-05-31 04:17:03'),
+(26, 'lrn2', '1625192366', '2021-07-02 02:19:16'),
+(27, 'LRN2', '1625192366', '2021-07-02 02:19:16');
 
 -- --------------------------------------------------------
 
@@ -265,7 +266,100 @@ INSERT INTO `logs` (`id`, `user`, `title`, `details`, `created_at`) VALUES
 (171, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-05 07:29:38'),
 (172, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-05 07:30:10'),
 (173, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-06-05 07:31:04'),
-(174, '', 'Student Logout', 'Student has been logout to the System.', '2021-06-05 07:41:23');
+(175, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-06-10 02:29:10'),
+(176, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-10 02:29:36'),
+(177, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-06-10 02:30:09'),
+(178, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-06-10 02:30:27'),
+(179, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-10 02:42:57'),
+(180, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-06-10 02:43:02'),
+(181, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-10 02:44:43'),
+(182, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-06-10 13:49:06'),
+(183, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-06-10 13:49:12'),
+(184, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-15 04:24:18'),
+(185, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-15 04:29:39'),
+(186, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-15 04:29:50'),
+(187, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-15 04:30:04'),
+(188, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-15 04:30:15'),
+(189, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-15 04:35:40'),
+(190, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-15 04:41:04'),
+(191, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-16 07:02:18'),
+(192, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-06-16 07:04:18'),
+(193, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-16 07:04:20'),
+(194, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-06-16 07:08:35'),
+(195, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-16 14:48:18'),
+(196, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-16 14:48:18'),
+(197, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-06-16 14:48:43'),
+(198, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-16 14:49:05'),
+(199, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-06-16 14:49:14'),
+(200, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-06-16 14:51:05'),
+(201, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-16 14:51:23'),
+(202, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-18 17:21:19'),
+(203, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-21 13:52:20'),
+(204, '2021', 'New Module', 'Added a new Module to the System.', '2021-06-21 13:52:58'),
+(205, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-21 14:34:43'),
+(206, '2022', 'Teacher Login', 'Jan Michael Espiritu is now Login to the system', '2021-06-21 14:34:46'),
+(207, '2022', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-21 14:35:35'),
+(208, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-21 14:35:42'),
+(209, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-21 14:35:46'),
+(210, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-21 14:35:49'),
+(211, '2021', 'New Video Presentation', 'Added a new Video Presentation to the System.', '2021-06-21 15:25:51'),
+(212, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-24 13:22:25'),
+(213, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-24 13:22:25'),
+(214, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-06-24 13:23:01'),
+(215, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-24 13:53:20'),
+(216, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-06-24 13:53:25'),
+(218, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-26 14:55:57'),
+(219, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-26 15:00:14'),
+(220, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-26 15:10:35'),
+(221, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-26 15:37:12'),
+(222, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-06-26 15:37:15'),
+(223, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-06-26 15:37:26'),
+(224, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-26 15:37:28'),
+(225, 'admin', 'New Deleted', 'Deleted a Module to the System.', '2021-06-26 15:54:51'),
+(226, 'admin', 'New Deleted', 'Deleted a Module to the System.', '2021-06-26 15:55:14'),
+(227, '2021', 'New Module', 'Added a new Module to the System.', '2021-06-26 15:55:31'),
+(228, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-26 17:09:03'),
+(229, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-26 17:09:27'),
+(230, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-26 17:22:10'),
+(231, '2022', 'Teacher Login', 'Jan Michael Espiritu is now Login to the system', '2021-06-26 17:22:15'),
+(232, 'admin', 'New Deleted', 'Deleted a Student to the System.', '2021-06-26 17:23:22'),
+(233, '2022', 'New Student', 'Added a new Student to the System.', '2021-06-26 17:24:55'),
+(234, 'admin', 'New Deleted', 'Deleted a Student to the System.', '2021-06-26 17:25:31'),
+(235, '2022', 'New Student', 'Added a new Student to the System.', '2021-06-26 17:26:00'),
+(236, 'lrn2', 'Student Login', 'Princess is now Login to the system', '2021-06-26 17:26:27'),
+(237, 'lrn2', 'Student Logout', 'Student has been logout to the System.', '2021-06-26 17:26:39'),
+(238, '2022', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-06-26 17:27:10'),
+(239, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-26 17:27:13'),
+(240, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-29 15:43:36'),
+(241, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-06-29 15:43:37'),
+(242, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-07-01 05:00:18'),
+(243, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-07-01 05:00:46'),
+(244, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-07-01 05:08:32'),
+(245, 'lrn2', 'Student Login', 'Princess is now Login to the system', '2021-07-01 05:08:41'),
+(246, 'lrn2', 'Student Logout', 'Student has been logout to the System.', '2021-07-01 05:10:32'),
+(247, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-07-01 05:10:39'),
+(248, '2022', 'Teacher Login', 'Jan Michael Espiritu is now Login to the system', '2021-07-01 05:10:45'),
+(249, '2022', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-07-01 05:11:00'),
+(250, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-07-01 05:11:09'),
+(251, 'admin', 'New Deleted', 'Deleted a Disability to the System.', '2021-07-01 05:16:54'),
+(252, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-07-01 05:17:07'),
+(253, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-07-01 05:17:17'),
+(254, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-07-01 05:17:26'),
+(255, 'lrn2', 'Student Login', 'Princess is now Login to the system', '2021-07-01 05:17:33'),
+(256, 'lrn2', 'Student Logout', 'Student has been logout to the System.', '2021-07-01 05:17:51'),
+(257, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-07-01 05:24:08'),
+(258, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-07-02 02:03:05'),
+(259, 'admin', 'Admin Logout', 'Admin has been logout to the System.', '2021-07-02 02:03:11'),
+(260, 'lrn1', 'Student Login', 'Shanne Marie Capinpin is now Login to the system', '2021-07-02 02:13:58'),
+(261, 'lrn1', 'Student Logout', 'Student has been logout to the System.', '2021-07-02 02:14:27'),
+(262, 'lrn2', 'Student Login', 'Princess is now Login to the system', '2021-07-02 02:18:57'),
+(263, 'lrn2', 'Student Logout', 'Student has been logout to the System.', '2021-07-02 02:19:16'),
+(264, '2021', 'Teacher Login', 'Gian Carino is now Login to the system', '2021-07-02 02:19:21'),
+(265, '2021', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-07-02 02:35:37'),
+(266, '2022', 'Teacher Login', 'Jan Michael Espiritu is now Login to the system', '2021-07-02 02:35:44'),
+(267, '2022', 'Teacher Logout', 'Teacher has been logout to the System.', '2021-07-02 02:35:47'),
+(268, 'Admin', 'Admin Login', 'Administrator is now Login to the system', '2021-07-02 02:35:51'),
+(269, 'admin', 'Deleted Teacher', 'Deleted a Teacher to the System.', '2021-07-02 02:36:03');
 
 -- --------------------------------------------------------
 
@@ -288,8 +382,8 @@ CREATE TABLE `module` (
 --
 
 INSERT INTO `module` (`id`, `disability`, `title`, `teacher_id`, `file`, `view`, `created_at`) VALUES
-(11, '1', 'Mathematics', '2021', 'me.docx', '6', '2021-05-30 02:47:02'),
-(12, '2', 'English', '2022', 'us.docx', '4', '2021-05-30 02:48:20');
+(12, '2', 'English', '2022', 'us.docx', '3', '2021-05-30 02:48:20'),
+(14, '1', 'Mathematics', '2021', 'me.docx', '0', '2021-06-26 23:55:31');
 
 -- --------------------------------------------------------
 
@@ -310,16 +404,7 @@ CREATE TABLE `module_view` (
 --
 
 INSERT INTO `module_view` (`id`, `module_id`, `teacher_id`, `lrn_id`, `created_at`) VALUES
-(69, '11', '2021', 'lrn1', '2021-05-29 19:00:54'),
-(70, '12', '2022', 'lrn2', '2021-05-29 19:00:56'),
-(74, '12', '2022', 'lrn2', '2021-05-29 19:16:18'),
-(75, '11', '2021', 'lrn1', '2021-05-29 19:16:48'),
-(76, '11', '2021', 'lrn1', '2021-05-29 19:17:09'),
-(77, '12', '2022', 'lrn2', '2021-06-02 06:26:31'),
-(78, '12', '2022', 'lrn2', '2021-06-02 06:27:38'),
-(79, '11', '2021', 'lrn1', '2021-06-05 07:23:58'),
-(80, '11', '2021', 'lrn1', '2021-06-05 07:24:16'),
-(81, '11', '2021', 'lrn1', '2021-06-05 07:24:25');
+(86, '12', '2022', 'lrn2', '2021-07-02 02:19:09');
 
 -- --------------------------------------------------------
 
@@ -346,9 +431,8 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `lrn`, `type`, `picture`, `fullname`, `disability`, `contact_no`, `address`, `gender`, `status`, `created_at`) VALUES
-(9, 'LRN1', 'student', '1622313240_1575459576_nonales.png', 'Shanne Marie Capinpin', '1', 'lrn1', 'urdaneta, pangasinan', 'Female', 'activated', '2021-06-02 06:19:38'),
-(10, 'LRN2', 'student', '1622313286_1567571907_deguzman.png', 'Princess Prado', '2', 'lrn2', 'Pangasinan', 'Female', 'activated', '2021-06-02 06:36:11'),
-(11, 'wew', 'student', '1622434545_1620871063_email-309491.png', 'wew', '4', 'wew', 'wew', 'Male', 'activated', '2021-05-31 04:15:45');
+(9, 'LRN1', 'student', '1623735082_1622313240_1575459576_nonales.png', 'Shanne Marie Capinpin', '1', 'lrn1', 'urdaneta, pangasinan', 'Female', 'activated', '2021-06-26 17:10:16'),
+(13, 'LRN2', 'student', '1624728360_1622313286_1567571907_deguzman.png', 'Princess', '2', 'LRN2', 'Pangasinan', 'Female', 'activated', '2021-06-26 17:26:00');
 
 -- --------------------------------------------------------
 
@@ -377,8 +461,7 @@ CREATE TABLE `teacher` (
 INSERT INTO `teacher` (`id`, `teacher_id`, `type`, `picture`, `fullname`, `contact_no`, `email`, `address`, `gender`, `status`, `created_at`) VALUES
 (11, '2021', 'teacher', '1622312706_1620824045_1569425601_cry.jpg', 'Gian Carino', '2021', 'gian@gmail.com', 'Alcala, Pangasinan', 'Male', 'activated', '2021-05-29 18:25:07'),
 (12, '2022', 'teacher', '1622312746_1620823696_1575495009_myAvatar.png', 'Jan Michael Espiritu', '2022', 'janong@gmail.com', 'Pozorubio, Pangasinan', 'Male', 'activated', '2021-05-29 18:25:46'),
-(13, '2023', 'teacher', '1622312778_1620823988_1567591139_micua.jpg', 'Ramel Macaraeg', '2023', 'ramel@gmail.com', 'Alcala, Pangasinan', 'Male', 'activated', '2021-05-29 18:26:19'),
-(14, 'asd', 'teacher', '1622434383_1622131308_placeholder.png', 'asd', 'asd', 'asd@gmail.com', 'asd', 'Male', 'activated', '2021-05-31 04:13:03');
+(13, '2023', 'teacher', '1622312778_1620823988_1567591139_micua.jpg', 'Ramel Macaraeg', '2023', 'ramel@gmail.com', 'Alcala, Pangasinan', 'Male', 'activated', '2021-05-29 18:26:19');
 
 -- --------------------------------------------------------
 
@@ -408,9 +491,7 @@ INSERT INTO `user` (`id`, `identification`, `username`, `password`, `picture`, `
 (240, '2022', '2022', '$2y$10$zyYAhnNaoIWAer/4lVzzmOyAWc1mfyy34CoCYRjCwKVtvDoYuoL1u', '1622312746_1620823696_1575495009_myAvatar.png', 'teacher', 'approved', '2022', '2021-05-29 18:25:46'),
 (241, '2023', '2023', '$2y$10$AOoAljsW9yYHPU0Qj98RHOARFGJCQNeQqHCGrbFIXgngYgTJLUxuW', '1622312778_1620823988_1567591139_micua.jpg', 'teacher', 'approved', '2023', '2021-05-29 18:26:19'),
 (242, 'LRN1', 'LRN1', '$2y$10$lDrRkA6LOwJzK2ibicUQSeoUL3xjbUCU4K1jOIVUCcHf2udH4aEaO', '1622313240_1575459576_nonales.png', 'student', 'approved', 'lrn1', '2021-05-29 18:34:00'),
-(243, 'LRN2', 'LRN2', '$2y$10$qtOmNquF2GYaUC7fwTxd4OLh0vXd34OMQGv3GYSX1kHt3OSyMDenq', '1622313286_1567571907_deguzman.png', 'student', 'approved', 'lrn2', '2021-05-29 18:34:46'),
-(244, 'asd', 'asd', '$2y$10$fAUIO6TqJTc2CD3zFq5vGeDaEb5PVWHE9gQ.qsr3DaVJLo1/rvsUG', '1622434383_1622131308_placeholder.png', 'teacher', 'approved', 'asd', '2021-05-31 04:13:03'),
-(245, 'wew', 'wew', '$2y$10$5KJwnlcM7C7vU.CalK0JG.drLTZRArQgabEd71p80JBnRGmovH/EC', '1622434545_1620871063_email-309491.png', 'student', 'approved', 'wew', '2021-05-31 04:15:45');
+(247, 'LRN2', 'LRN2', '$2y$10$MvF2aqiq3bnbybbCtPyYa.lpD4MwZkZhW6TjD5p2ZYpDACg8Tz7Qa', '1624728360_1622313286_1567571907_deguzman.png', 'student', 'approved', 'LRN2', '2021-06-26 17:26:00');
 
 -- --------------------------------------------------------
 
@@ -432,7 +513,8 @@ CREATE TABLE `video` (
 --
 
 INSERT INTO `video` (`id`, `disability`, `title`, `teacher_id`, `video`, `created_at`) VALUES
-(10, '1', 'For the kids', '2021', '1611050902_Components of Physical Fitness (MAPEH Educational Video).mp4', '2021-05-29 18:40:54');
+(10, '1', 'For the kids', '2021', '1611050902_Components of Physical Fitness (MAPEH Educational Video).mp4', '2021-05-29 18:40:54'),
+(11, '1', 'test', '2021', 'videoplayback.mp4', '2021-06-21 15:25:51');
 
 --
 -- Indexes for dumped tables
@@ -578,31 +660,31 @@ ALTER TABLE `flash_cards_title`
 -- AUTO_INCREMENT for table `login_details`
 --
 ALTER TABLE `login_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=270;
 
 --
 -- AUTO_INCREMENT for table `module`
 --
 ALTER TABLE `module`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `module_view`
 --
 ALTER TABLE `module_view`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `teacher`
@@ -614,13 +696,13 @@ ALTER TABLE `teacher`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=248;
 
 --
 -- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
